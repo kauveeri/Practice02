c9c21fd666bd5af5d5152b776ebcdaa16119e2f9
kauveeri <kauveeri@sheridancollege.ca>
2022-02-13 22:22:57 -0500

    Practice 1

---
+++ src/practice/Practice.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practice;
+
+import java.util.Random;
+import java.util.Scanner;
+import static practice.Subclass.SUITS;
+
+/**
+ *
+ * @author Jassu01
+ */
+public class Practice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       Subclass[] s = new Subclass[10];
+       
+       Random random= new Random();
+       
+       for (int i=0;i<s.length;i++){
+           
+           Subclass s1 = new Subclass ();
+           s1.setNumbers(1+(int)(Math.random()*13));
+               int randomNumber =s1.next(SUITS.length);
+          s1.setSuit(SUITS[randomNumber]);
+          
+          System.out.println(s1.getNumbers() +" " + s1.getSuit());
+           s[i] = s1;
+   
+       }
+       
+       
+    }
+}
---
+++ src/practice/Subclass.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practice;
+
+/**
+ *
+ * @author Jassu01
+ */
+public class Subclass {
+    
+    
+    private String Suit;
+    private int numbers;
+    public static String[] SUITS ={"diamonds","spades","clubs","hearts"};
+
+    /**
+     * @return the Suit
+     */
+    public String getSuit() {
+        return Suit;
+    }
+
+    /**
+     * @param Suit the Suit to set
+     */
+    public void setSuit(String Suit) {
+        this.Suit = Suit;
+    }
+
+    /**
+     * @return the numbers
+     */
+    public int getNumbers() {
+        return numbers;
+    }
+
+    /**
+     * @param numbers the numbers to set
+     */
+    public void setNumbers(int numbers) {
+        this.numbers = numbers;
+    }
+
+    int next(int length) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
